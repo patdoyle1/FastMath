5ca54bbb5c7a3d84b98a167bec9af86e1433cc38
Anders Maraviglia <anders_Maraviglia@yahoo.com>
2014-03-13 15:40:26 -0400

    linked tesseract and wolfram in test.py (needs to be renamed), and set up preliminary test suite of pictures of mathmatical problems to run through the new system.  Initial results indicate ttesseract needs a lot of training to recognise more than just the basic mathmatical operators

--- FastMathServer/test.py
+++ FastMathServer/test.py
@@ -1,6 +1,8 @@
 #from __future__ import unicode_literals
 import wolframalpha
 import sys
+import os
+import subprocess
 
 app_id = 'TGR8PR-V28TUUGJWY'
 "App ID for testing this project. Please don't use for other apps."
@@ -9,15 +11,27 @@ def parse_question(q):
 	q = q.replace('"', '^')
 	return q
 
+def parse_output(res, question):
+	foundAns = False
+	for pod in res:
+		if foundAns is True:
+			return pod.text
+		foundAns = True
+
 if __name__ == "__main__":
-	output = str(sys.argv[1])
-	question = open(output).readline().strip()
+	imageInputFileName = "image-tests\\testMultiply.PNG"
+	tesseractQuery = 'Tesseract-OCR\\tesseract.exe ' + imageInputFileName + ' input' # this command will be different for macs and unix systems
+	os.system(tesseractQuery) # send command to terminal 
+	inputFileName = "input.txt" # name of file which holds the query in text form 
+	question = open(inputFileName).readline().strip()
 	question = parse_question(question)
-	
-	client = wolframalpha.Client(app_id)
-	res = client.query(question)
+	client = wolframalpha.Client(app_id) 
+	res = client.query(question) # send query to wolfram 
+	#print(parse_output(res, question)) # prints the final output to the question (alternatives avalible, but for debugging we'll stick with this)
 	for pod in res:
 		print pod.text
+	
+	
 
 
 		
