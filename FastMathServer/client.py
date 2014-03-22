7ce84fb8e1c710cfba9088037a3922811f6b288d
Pat Doyle <doylep5@rpi.edu>
2014-03-22 07:23:28 -0400

    Changed server code to hopefully make getting tesseract output easier. also played with inputs.txt file

--- FastMathServer/client.py
+++ FastMathServer/client.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python           # This is client.py file
 
-import socket               # Import socket module
+import socket, os, sys               # Import socket module
+
+
+def parse_output(res, question):
+	foundAns = False
+	for pod in res:
+		if foundAns is True:
+			return pod.text
+		foundAns = True
+
+def parse_question(q):
+	q = q.replace('"', '^')
+	return q
+
+
+size = 512
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 
-s.connect((host, port))
+s.connect((host, port)) # connects to the server
+
+
+fname = os.system(sys.argv[1]) # this is the picture being passed in
+s.send(fname) #sends the picture to the server
+
 print s.recv(1024)
+
 s.close                     # Close the socket when done
--- FastMathServer/socketserver.py
+++ FastMathServer/socketserver.py
@@ -1,4 +1,4 @@
-   #from __future__ import unicode_literals
+#from __future__ import unicode_literals
 import wolframalpha
 import sys
 import os
@@ -13,43 +13,62 @@ host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
+requests = "inputs.txt" #potential way to store all of the requests that come in
 
+def parse_output(res, question):
+	foundAns = False
+	for pod in res:
+		if foundAns is True:
+			return pod.text
+		foundAns = True
+
+def parse_question(q):
+	q = q.replace('"', '^')
+	return q
 
 
 
 s.listen(5)                 # Now wait for client connection. (can only hold query of 5)
 while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   data = client_socet.recv(512)   # 512 is the number of bytes
-
-	if __name__ == "__main__":
-		imageInputFileName = open(data, 'rb') 
-		tesseractQuery = 'Tesseract-OCR\\tesseract.exe ' + imageInputFileName + ' input' # this command will also be different for unix systems
-		os.system(tesseractQuery) # send command to terminal 
-		#may want to ahve teh input.txt file on the server but not sure yet, will collaborate with group later
-		client = wolframalpha.Client(app_id) 
-		res = client.query(question) # send query to wolfram 
-		c.send(res)
-
-		
-		
-
-   c.close()                # Close the connection
-
-
-
-
-
-
-
-
-
-
-
-	
-	
+    c, addr = s.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    c.send('Thank you for connecting')
+    data = client_socet.recv(512)   # 512 is the number of bytes
+        
+        if __name__ == "__main__":
+            imageInputFileName = open(data, 'rb')
+            tesseractQuery = 'Tesseract-OCR\\tesseract.exe ' + imageInputFileName + ' input' # this command will also be different for unix systems
+            os.system(tesseractQuery))) # send command to terminal
+            question = subprocess.check_output(os.system(tesseractQuery))#sets question to whatever tesseract spits out
+            
+            parse_question(question)
+            
+            query = open('inputs.txt',w) #opens input and writes the question to it
+            query.write(question "\n")
+            query.close()
+            
+            client = wolframalpha.Client(app_id)
+            res = client.query(question) # send query to wolfram
+            res = parse_output(res,question)
+            
+            c.send(res)
+    
+    
+    
+    
+    c.close()                # Close the connection
 
 
-		
+
+
+
+
+
+
+
+
+
+
+
+
+
